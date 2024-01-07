b376d1c562735ac50cdaa768fb586d5e42660b27
laggedSkapari <es.rajatyadav@gmail.com>
2024-01-07 12:39:16 +0530

    + added Database services (Task Services)

--- cross/cross_v2/lib/data/database_services.dart
+++ cross/cross_v2/lib/data/database_services.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:cross_v2/data/task_list.dart';
@@ -12,9 +14,10 @@ class DatabaseServices {
   }
 
   Future<Isar> openDatabase() async {
-    if(Isar.instanceNames.isEmpty){
+    if (Isar.instanceNames.isEmpty) {
       final dir = await getApplicationDocumentsDirectory();
-      final isar = await Isar.open([TaskListSchema, TaskSchema], directory: dir.path);
+      final isar =
+          await Isar.open([TaskListSchema, TaskSchema], directory: dir.path);
       return isar;
     }
     return Future.value(Isar.getInstance());
@@ -22,14 +25,15 @@ class DatabaseServices {
 
   Future<List<TaskList>> loadAllTaskLists() async {
     final Isar dbInstance = await _db;
-    final taskLists = await dbInstance.taskLists.where().sortByTaskListTitleDesc().findAll();
+    final taskLists =
+        await dbInstance.taskLists.where().sortByTaskListTitleDesc().findAll();
     return taskLists;
   }
 
   Future<void> createNewTaskList({required String tasktitle}) async {
     final Isar dbInstance = await _db;
     const uuid = Uuid();
-    final taskList = TaskList(taskListId: uuid.v4() , taskListTitle: tasktitle);
+    final taskList = TaskList(taskListId: uuid.v4(), taskListTitle: tasktitle);
     await dbInstance.writeTxn(() async {
       await dbInstance.taskLists.put(taskList);
     });
@@ -37,10 +41,74 @@ class DatabaseServices {
 
   Future<void> deleteTaskList(String taskListId) async {
     final Isar dbInstance = await _db;
-    final TaskList? taskList = await dbInstance.taskLists.filter().idEqualTo(taskListId).findFirst();
-    if(taskList != null){
+    final TaskList? taskList =
+        await dbInstance.taskLists.filter().idEqualTo(taskListId).findFirst();
+    if (taskList != null) {
       await dbInstance.writeTxn(() async {
         await dbInstance.taskLists.delete(taskList.id);
+      });
+    }
+  }
+  //Task Services
+
+  Future<List<Task>> loadAllTasks() async {
+    final Isar dbInstance = await _db;
+    final tasks =
+        await dbInstance.tasks.where().sortByIsImportantDesc().findAll();
+    return tasks;
+  }
+
+  Future<void> createNewTask({required String id}) async {
+    final Isar dbInstance = await _db;
+    final Task? task =
+        await dbInstance.tasks.filter().idEqualTo(id).findFirst();
+    if (task != null) {
+      dbInstance.writeTxn(() async {
+        await dbInstance.tasks.put(task);
+      });
+    }
+  }
+
+  Future<void> deleteTask({required String id}) async {
+    final Isar dbInstance = await _db;
+    final Task? task =
+        await dbInstance.tasks.filter().idEqualTo(id).findFirst();
+    if(task != null){
+      dbInstance.writeTxn(() async {
+        await dbInstance.tasks.delete(task.id);
+      });
+    }
+  }
+
+  Future<void> crossTask({required String id}) async {
+    final Isar dbInstance = await _db;
+    final Task? task = await dbInstance.tasks.filter().idEqualTo(id).findFirst();
+    if(task != null){
+      dbInstance.writeTxn(() async {
+        task.isCompleted = true;
+       await dbInstance.tasks.put(task);
+      });
+    }
+  }
+
+  Future<void> unCrossTask({required String id}) async {
+    final Isar dbInstance = await _db;
+    final Task? task = await dbInstance.tasks.filter().idEqualTo(id).findFirst();
+    if(task != null){
+      dbInstance.writeTxn(() async {
+        task.isCompleted = false;
+       await dbInstance.tasks.put(task);
+      });
+    }
+  }
+
+Future<void> toggleTaskImportance({required String id}) async {
+    final Isar dbInstance = await _db;
+    final Task? task = await dbInstance.tasks.filter().idEqualTo(id).findFirst();
+    if(task != null){
+      dbInstance.writeTxn(() async {
+       task.isImportant = !task.isImportant;
+       await dbInstance.tasks.put(task);
       });
     }
   }
