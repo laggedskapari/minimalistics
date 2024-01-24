4fe7fdcce9f3d344e85fb48debb60523468c1836
laggedSkapari <es.rajatyadav@gmail.com>
2024-01-24 22:24:53 +0530

    fixed Navigation bar.

--- cross/cross_v2/lib/presentation/widgets/cross_navigation_bar.dart
+++ cross/cross_v2/lib/presentation/widgets/cross_navigation_bar.dart
@@ -17,85 +17,53 @@ class _CrossNavigationBarState extends State<CrossNavigationBar> {
   Widget build(BuildContext context) {
     double displayWidth = MediaQuery.of(context).size.width;
     return Container(
+
       margin: EdgeInsets.symmetric(
-          horizontal: displayWidth * .136, vertical: displayWidth * .05),
+        vertical: displayWidth * .05,
+      ),
       height: displayWidth * .110,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(.1),
-            blurRadius: 30,
-            offset: const Offset(0, 10),
-          ),
-        ],
-        borderRadius: BorderRadius.circular(50),
       ),
-      child: ListView.builder(
-        itemCount: 3,
-        scrollDirection: Axis.horizontal,
-        padding: EdgeInsets.symmetric(horizontal: displayWidth * .02),
-        itemBuilder: (context, index) => InkWell(
-          onTap: () {
-            setState(() {
-              currentIndex = index;
-              widget.changeHomePage(index);
-              HapticFeedback.lightImpact();
-            });
-          },
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          child: Stack(
-            children: [
-              AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                curve: Curves.fastLinearToSlowEaseIn,
-                width: displayWidth * .23,
-                alignment: Alignment.center,
-                child: AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.fastLinearToSlowEaseIn,
-                  height: displayWidth * .09,
-                  width: displayWidth * .40,
-                  decoration: BoxDecoration(
-                    color: index == currentIndex
-                        ? Theme.of(context).colorScheme.secondary.withOpacity(.2)
-                        : Colors.transparent,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-              AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                curve: Curves.fastLinearToSlowEaseIn,
-                width: displayWidth * .20,
-                alignment: Alignment.center,
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          curve: Curves.fastLinearToSlowEaseIn,
-                          width: displayWidth * .06,
-                        ),
-                        Text(
-                          listOfStrings[index],
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'JetBrainsMono',
-                            fontWeight: FontWeight.bold,
-                            color: index == currentIndex
-                                ? Theme.of(context).colorScheme.primary
-                                : Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                      ],
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: displayWidth * .2),
+        alignment: Alignment.center,
+        child: ListView.builder(
+          itemCount: 3,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: displayWidth * .02),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(() {
+                currentIndex = index;
+                widget.changeHomePage(index);
+                HapticFeedback.lightImpact();
+              });
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      listOfStrings[index],
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'JetBrainsMono',
+                        fontWeight: FontWeight.bold,
+                        color: index == currentIndex
+                            ? Theme.of(context).colorScheme.primary
+                            : Theme.of(context).colorScheme.secondary,
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
