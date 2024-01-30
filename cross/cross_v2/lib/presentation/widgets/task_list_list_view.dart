4944ee84b65af5132fa33e20b63680bc8fb164e6
laggedSkapari <es.rajatyadav@gmail.com>
2024-01-30 21:40:15 +0530

    UI Changes.

--- cross/cross_v2/lib/presentation/widgets/task_list_list_view.dart
+++ cross/cross_v2/lib/presentation/widgets/task_list_list_view.dart
@@ -1,6 +1,5 @@
 import 'package:cross_v2/domain/bloc/Task/task_bloc.dart';
 import 'package:cross_v2/domain/bloc/TaskList/task_list_bloc.dart';
-import 'package:cross_v2/presentation/widgets/task_list_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,27 +16,43 @@ class _TaskListListViewState extends State<TaskListListView> {
   int currentIndex = -1;
   @override
   Widget build(BuildContext context) {
+    double displayWidth = MediaQuery.of(context).size.width;
     return BlocBuilder<TaskListBloc, TaskListState>(
       builder: (context, state) {
         if (state is TaskListsLoadedState) {
-          return ListView.builder(
-            itemBuilder: (context, index) => InkWell(
-              onTap: () {
-                setState(() {
-                  currentIndex = index;
-                  BlocProvider.of<TaskBloc>(context)
-                      .add(LoadTasksEvent(taskList: state.taskLists[index].id));
-                });
-              },
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              child: Text(
-                state.taskLists[index].taskListTitle,
-                style: TextStyle(color: currentIndex == index ? Colors.red : Colors.teal),
+          return Container(
+            margin: EdgeInsets.symmetric(horizontal: displayWidth * 0.16),
+            child: ListView.builder(
+              itemBuilder: (context, index) => InkWell(
+                onTap: () {
+                  setState(
+                    () {
+                      currentIndex = index;
+                      BlocProvider.of<TaskBloc>(context).add(
+                        LoadTasksEvent(taskList: state.taskLists[index].id),
+                      );
+                    },
+                  );
+                },
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  child: Text(
+                    '[${state.taskLists[index].taskListTitle.toUpperCase()}]',
+                    style: TextStyle(
+                      color: currentIndex == index
+                          ? Theme.of(context).colorScheme.primary
+                          : Theme.of(context).colorScheme.secondary,
+                      fontSize: 20,
+                      fontWeight: currentIndex == index ? FontWeight.w900 : FontWeight.normal,
+                    ),
+                  ),
+                ),
               ),
+              scrollDirection: Axis.horizontal,
+              itemCount: state.taskLists.length,
             ),
-            scrollDirection: Axis.horizontal,
-            itemCount: state.taskLists.length,
           );
         }
         return const CircularProgressIndicator();
