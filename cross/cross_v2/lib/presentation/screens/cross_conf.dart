73f7b86179269d5de6534e96e58b0637ee839be9
laggedSkapari <es.rajatyadav@gmail.com>
2024-02-09 20:57:20 +0530

    added cross instructions.

--- cross/cross_v2/lib/presentation/screens/cross_conf.dart
+++ cross/cross_v2/lib/presentation/screens/cross_conf.dart
@@ -1,5 +1,6 @@
 import 'package:cross_v2/data/database_services.dart';
 import 'package:cross_v2/domain/bloc/TaskList/task_list_bloc.dart';
+import 'package:cross_v2/presentation/widgets/Conf/cross_commands.dart';
 import 'package:cross_v2/presentation/widgets/Conf/developer_info.dart';
 import 'package:cross_v2/presentation/widgets/Conf/task_list_conf_card.dart';
 import 'package:cross_v2/presentation/widgets/Conf/theme_preview_card.dart';
@@ -23,6 +24,7 @@ class CrossConf extends StatelessWidget {
               children: [
                 ThemePreviewCard(),
                 TaskListsConfCard(),
+                CrossCommands(),
                 DeveloperInfo(),
               ],
             ),
--- cross/cross_v2/lib/presentation/widgets/Conf/cross_commands.dart
+++ cross/cross_v2/lib/presentation/widgets/Conf/cross_commands.dart
@@ -1,1 +1,115 @@
+import 'package:flutter/material.dart';
 
+class CrossCommands extends StatelessWidget {
+  const CrossCommands({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '//CROSS COMMANDS',
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '//SWIPE RIGHT [ON TASK TITLE] -> ',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                'Complete task.',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '//DOUBLE TAP -> ',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                'Undo complete task.',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '//LONG TAP -> ',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                'Toggle Priority',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '//SWIPE LEFT -> ',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                'Delete task.',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 12,
+                  fontFamily: 'JetBrainsMono',
+                  fontWeight: FontWeight.w900,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
