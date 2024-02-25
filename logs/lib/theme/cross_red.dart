bd0b73f33d2ac418fec7bc6bbe3a41096a23f4fc
laggedSkapari <es.rajatyadav@gmail.com>
2024-02-25 21:17:57 +0530

    added Themes

---
+++ logs/lib/theme/cross_red.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+final crossRed = ThemeData(
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark,
+    primary: Color.fromARGB(255, 255, 54, 13),
+    onPrimary: Color.fromARGB(255, 255, 255, 255),
+    secondary: Color.fromARGB(255, 183, 183, 183),
+    onSecondary: Color.fromARGB(255, 255, 255, 255),
+    error: Color.fromARGB(250, 240, 122, 122),
+    onError: Color.fromARGB(255, 255, 255, 255),
+    background: Color.fromARGB(255, 255, 255, 255),
+    onBackground: Color.fromARGB(255, 255, 54, 13),
+    surface: Color.fromARGB(255, 50, 52, 55),
+    onSurface: Color.fromARGB(255, 75, 89, 117),
+  ),
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      fontSize: 22,
+      fontFamily: 'JetBrainsMono',
+      fontWeight: FontWeight.w900,
+      color: Color.fromARGB(255, 255, 54, 13),
+    ),
+    titleMedium: TextStyle(
+      fontSize: 19,
+      fontFamily: 'JetBrainsMono',
+      fontWeight: FontWeight.w900,
+      color: Color.fromARGB(255, 255, 54, 13),
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 19,
+      fontFamily: 'JetBrainsMono',
+      color: Color.fromARGB(255, 183, 183, 183),
+    ),
+    labelMedium: TextStyle(
+      fontSize: 15,
+      fontFamily: 'JetBrainsMono',
+      fontWeight: FontWeight.w900,
+      color: Color.fromARGB(255, 75, 89, 117),
+    ),
+    labelSmall: TextStyle(
+      fontSize: 12,
+      fontFamily: 'JetBrainsMono',
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 75, 89, 117),
+    ),
+  ),
+);
---
+++ logs/lib/theme/cross_themes.dart
@@ -0,0 +1,3 @@
+export 'cross_serika_dark.dart';
+export 'cross_red.dart';
+export 'cross_blue.dart';
