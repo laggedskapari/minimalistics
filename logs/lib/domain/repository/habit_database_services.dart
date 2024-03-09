080579c4c9266e3da48b803ec0a49c013b27af61
laggedSkapari <es.rajatyadav@gmail.com>
2024-03-09 17:08:59 +0530

    added habit database services.

--- logs/lib/domain/repository/habit_database_services.dart
+++ logs/lib/domain/repository/habit_database_services.dart
@@ -1,5 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logs/data/log_habit.dart';
 
 class HabitDatabaseServices {
+  final CollectionReference _userRef =
+      FirebaseFirestore.instance.collection('users');
 
+  Future<void> addNewHabit({
+    required String username,
+    required LogHabit newHabit,
+  }) {
+    return _userRef.doc(username).collection('habits').add({
+      'title': newHabit.habitTitle,
+      'desc': newHabit.habitDescription,
+      'timePeriod': newHabit.timePeriod,
+      'daysLeft': newHabit.daysLeft,
+      'streaks': newHabit.streak,
+      'createdBy': newHabit.createdBy,
+      'createdOn': newHabit.createdOn,
+      'freezes': newHabit.freezes,
+      'isSuccess': newHabit.isSuccess,
+    });
+  }
+
+  Future<void> removeHabit({
+    required String username,
+    required String habitId,
+  }) {
+    return _userRef.doc(username).collection('habit').doc(habitId).delete();
+  }
+
+  Future<void> failHabit({required String username, required String habitId}) {
+    return _userRef
+        .doc(username)
+        .collection('habits')
+        .doc(habitId)
+        .update({'isSuccess': false})
+        .then((value) => print('Successfully updated doc.'))
+        .onError((error, _) => print(error));
+  }
 }
