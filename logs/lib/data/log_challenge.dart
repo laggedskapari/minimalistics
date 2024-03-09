21d376e68ef088e6614ef70e442a73736d7842f7
laggedSkapari <es.rajatyadav@gmail.com>
2024-03-09 19:27:25 +0530

    updated log challenges.

--- logs/lib/data/log_challenge.dart
+++ logs/lib/data/log_challenge.dart
@@ -1,16 +1,21 @@
 class LogChallenge {
   String challengeTitle;
   String challengeDescription;
-  Map<String, int> contenderScore = {};
-  String createdOn;
+  DateTime createdOn;
   String createdBy;
   int freezeCount;
+  int timePeriod;
+  int daysLeft;
+  List<String> contenderList;
 
   LogChallenge({
     required this.challengeTitle,
     required this.challengeDescription,
+    required this.daysLeft,
+    required this.timePeriod,
     required this.createdOn,
     required this.createdBy,
     required this.freezeCount,
+    required this.contenderList,
   });
 }
